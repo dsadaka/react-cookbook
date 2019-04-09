b3a8362cf656a664577db0ad087c18caf8ef9d19
Dan Sadaka <dan@web-site1.com>
2019-04-08 20:52:52 -0400

    Through Chapter 2, recipe 7

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Home from './components/Home/Home';
 import './App.css';
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Home />
       </div>
     );
   }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Home.css'
+
+class Home extends Component {
+	render() {
+		return <h1>I'm Not Home Component</h1>;
+	}
+}
+
+export default Home;
