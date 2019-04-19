0e866e9e44cc0000c8cffe136987cc013e78cb54
Dan Sadaka <dan@web-site1.com>
2019-04-19 09:53:31 -0400

    through Chapter 4 recipe 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,24 @@
 import React from 'react';
-import Popup from 'react-popup';
-import Helmet from 'react-helmet';
-
-// import Todo from './Todo/Todo';
-// import logo from '../shared/images/logo.svg';
-// import Home from './Home/Home';
-// import Timer from './Pomodoro/Timer';
-// import Coins from './Coins/Coins'
-// import Notes from './Notes/Notes';
-// import Animation from './Animation/Animation'
-// import Numbers from './Numbers/Numbers'
-import Person from './Person/Person';
+import { element } from 'prop-types';
 import Header from '../shared/components/layout/Header';
 import Content from '../shared/components/layout/Content';
 import Footer from '../shared/components/layout/Footer';
-
 import './App.css';
-import './Popup.css';
 
-const App = () => (
+const App = props => (
   <div className="App">
-    <Helmet
-      title="Person Information"
-      meta={[
-        { name: 'title', content: 'Person Information' },
-        { name: 'description', content: 'This recipe talks about React Helmet' }
-      ]}
-    />
-    <Header title="Person Information" />
+    <Header title="Routing" />
 
     <Content>
-      <Person />
+      {props.children}
     </Content>
 
     <Footer />
-
-    <Popup />
   </div>
 );
 
+App.propTypes = {
+  children: element
+};
 
 export default App;
